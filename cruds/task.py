eeb09432e2867923e181a097e68ae4c71292bf0a
senaly0304 <senaly.0304@gmail.com>
2024-07-16 23:02:36 +0900

    add_database

--- cruds/task.py
+++ cruds/task.py
@@ -1,64 +1,42 @@
-from typing import Optional
-from schemas import TaskCreate, TaskStatus, TaskUpdate
+from schemas import TaskCreate, TaskUpdate
+from sqlalchemy.orm import Session
+from models import Task
 
-class Task:
-  def __init__(
-      self,
-      id: int,
-      name: str,
-      description: Optional[str],
-      status: TaskStatus
-  ):
-    self.id = id
-    self.name = name
-    self.description = description
-    self.status = status
+def find_all(db: Session):
+  return db.query(Task).all()
 
-tasks = [
-  Task(1, "勉強", "前回の終了地点から", TaskStatus.UNCOMPLETED),
-  Task(2, "運動", None, TaskStatus.UNCOMPLETED),
-  Task(3, "買い物", None, TaskStatus.UNCOMPLETED),
-]
+def find_by_id(db: Session, id: int):
+  return db.query(Task).filter(Task.id == id).first()
 
-def find_all():
-  return tasks
+def find_by_name(db: Session, name: str):
+  return db.query(Task).filter(Task.name.like(f"%{name}%"))
 
-def find_by_id(id: int):
-  for task in tasks:
-    if task.id == id:
-      return task
-  return None
-
-def find_by_name(name: str):
-  filtered_tasks = []
-
-  for task in tasks:
-    if task.name in name:
-      filtered_tasks.append(task)
-  return filtered_tasks
-
-def create(task_create: TaskCreate):
+def create(db: Session, task_create: TaskCreate):
   new_task = Task(
-    len(tasks) + 1,
-    task_create.name,
-    task_create.description,
-    TaskStatus.UNCOMPLETED,
+    **task_create.model_dump()
   )
-  new_task.append(new_task)
+  db.add(new_task)
+  db.commit()
   return new_task
 
-def update(id: int, task_update: TaskUpdate):
-  for task in tasks:
-    if task.id == id:
-      task.name = task.name if task_update.name is None else task_update.name
-      task.description = task.description if task_update.description is None else task_update.description
-      task.status = task.status if task_update.status is None else task_update.status
-      return task
-  return None
+def update(db: Session, id: int, task_update: TaskUpdate):
+  task = find_by_id(db, id)
+  if task is None:
+    return None
+  
+  task.name = task.name if task_update.name is None else task_update.name
+  task.description = task.description if task_update.description is None else task_update.description
+  task.status = task.status if task_update.status is None else task_update.status
 
-def delete(id: int):
-  for i in range(len(tasks)):
-    if tasks[i].id == id:
-      delete_task = tasks.pop(i)
-      return delete_task
-  return None
+  db.add(task)
+  db.commit()
+  return task
+
+def delete(db: Session, id: int):
+  task = find_by_id(db, id)
+  if task is None:
+    return None
+  
+  db.delete(task)
+  db.commit()
+  return task
--- routers/task.py
+++ routers/task.py
@@ -1,42 +1,48 @@
-from fastapi import APIRouter, Path, Query, HTTPException
+from fastapi import APIRouter, Path, Query, HTTPException, Depends
 from cruds import task as task_cruds
 from schemas import TaskCreate, TaskUpdate, TaskResponse
 from starlette import status
+from database import get_db
+from typing import Annotated
+from sqlalchemy.orm import Session
+
+# dbDependencyはsqlalchemyのセッション型となり、get_db関数から取得されるセッションが注入される
+dbDependency = Annotated[Session, Depends(get_db)]
 
 router = APIRouter(prefix="/tasks", tags=["Tasks"])
 
 @router.get("", response_model=list[TaskResponse], status_code=status.HTTP_200_OK)
-async def find_all():
-  return task_cruds.find_all()
+async def find_all(db: dbDependency):
+  return task_cruds.find_all(db)
 
 @router.get("/{id}", response_model=TaskResponse, status_code=status.HTTP_200_OK)
-async def find_by_id(id: int=Path(gt=0)):
-  found_task = task_cruds.find_by_id(id)
+async def find_by_id(db: dbDependency, id: int=Path(gt=0)):
+  found_task = task_cruds.find_by_id(db, id)
   if not found_task:
     raise HTTPException(status_code=404, detail="Task noy found")
   return found_task
 
 @router.get("/", response_model=list[TaskResponse], status_code=status.HTTP_200_OK)
-async def find_by_name(name: str = Query(min_length=1, max_length=20)):
-  return task_cruds.find_by_name(name)
+async def find_by_name(db: dbDependency, name: str = Query(min_length=1, max_length=20)):
+  return task_cruds.find_by_name(db, name)
 
 @router.post("", response_model=TaskResponse, status_code=status.HTTP_201_CREATED)
 # task_create=Body()とすることで、リクエストのBody要素をcreate-itemとして利用していたが、
 # スキーマをcreate関数の引数の型として指定することで、
 # リクエストのBodyの内容がスキーマの制約に適合しているかの自動チェックが入る
-async def create(task_create: TaskCreate):
-  return task_cruds.create(task_create)
+async def create(db: dbDependency, task_create: TaskCreate):
+  return task_cruds.create(db, task_create)
 
 @router.put("/{id}", response_model=TaskResponse, status_code=status.HTTP_200_OK)
-async def update(task_update: TaskUpdate, id: int=Path(gt=0)):
-  updated_task = task_cruds.update(id, task_update)
+async def update(db: dbDependency, task_update: TaskUpdate, id: int=Path(gt=0)):
+  updated_task = task_cruds.update(db, id, task_update)
   if not updated_task:
     raise HTTPException(status_code=404, detail="Task not updated")
   return updated_task
 
 @router.delete("/{id}", response_model=TaskResponse, status_code=status.HTTP_200_OK)
-async def delete(id: int=Path(gt=0)):
-  deleted_task = task_cruds.delete(id)
+async def delete(db: dbDependency, id: int=Path(gt=0)):
+  deleted_task = task_cruds.delete(db, id)
   if not deleted_task:
     raise HTTPException(status_code=404, detail="Task not deleted")
   return deleted_task
